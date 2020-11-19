9a96259be71144942ae0526e5198cbf28a704f99
daniil <savindaniil29@gmail.com>
2020-11-19 23:40:25 +0300

    файл 1 исправлен

--- 111.py
+++ 111.py
@@ -1,2 +1,11 @@
-print(1)
-print(2)
+class LabirintTurtle():
+    def load_map():
+        pass
+    def show_map():
+        pass
+    def check_map():
+        pass
+    def exit_count_step():
+        pass
+    def exit_show_step():
+        pass
